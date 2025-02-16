3c98875b07188c5be483c9198409b745d6072402
hiimsajo <hiimsajo@gmail.com>
2025-02-17 00:39:52 +0900

    fix: 시크릿키 환경변수에 저장해두고 사용하도록 수정

--- onboarding_assignment/src/main/java/com/example/onboarding_assignment/infrastructure/jwt/JwtTokenizer.java
+++ onboarding_assignment/src/main/java/com/example/onboarding_assignment/infrastructure/jwt/JwtTokenizer.java
@@ -1,66 +1,125 @@
 package com.example.onboarding_assignment.infrastructure.jwt;
 
+import com.example.onboarding_assignment.domain.model.UserRole;
+import com.example.onboarding_assignment.presentation.dto.responseDto.AuthorityDto;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.io.Encoders;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import jakarta.servlet.http.HttpServletRequest;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Map;
+import javax.crypto.SecretKey;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Slf4j
+@Component
 public class JwtTokenizer {
 
-  // SecretKey를 Base64 기반으로 인코딩하기
-  public String encodeSecretKey(String secretKey) {
-    return Encoders.BASE64.encode(secretKey.getBytes(StandardCharsets.UTF_8));
+  @Value("${service.jwt.access-expiration}")
+  private Long accessExpiration;
+
+  public static final String AUTHORIZATION_ROLE = "userRole";
+
+  private final SecretKey secretKey;
+
+  public JwtTokenizer(@Value("${service.jwt.secret-key}") String secretKey) {
+    this.secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
   }
 
   // accessToken 발급하기
-  public String generateAccessToken(Map<String, Object> claims,
-      String subject,
-      Date expiration,
-      String base64EncodedSecretKey) {
-    Key key = getKeyFromBase64EncodedKey(base64EncodedSecretKey); // Base64형식 Secret Key 문자열을 이용해 Key객체를 얻음
-
+  public String generateAccessToken(String userRole,
+      String username) {
     return Jwts.builder()
-        .setClaims(claims)          // JWT에 포함 시킬 Custom Claims를 추가함 (Custom Claims에는 주로 인증된 사용자와 관련된 정보를 추가)
-        .setSubject(subject)        // JWT에 대한 제목을 추가
+        .claim(AUTHORIZATION_ROLE, "ROLE_" + userRole)          // JWT에 포함 시킬 Claims(role)
+        .setSubject(username)        // JWT에 대한 제목을 추가
         .setIssuedAt(Calendar.getInstance().getTime())   // JWT 발행 일자를 설정, 파라미터 타입은 java.util.Date 타입
-        .setExpiration(expiration)  // JWT의 만료일시를 지정
-        .signWith(key)              // 서명을 위한 Key 객체를 설정
+        .setExpiration(new Date(System.currentTimeMillis() + accessExpiration))  // JWT의 만료일시를 지정
+        .signWith(secretKey)              // 서명을 위한 Key 객체를 설정
         .compact();                 // JWT를 생성하고 직렬화함
   }
 
   // accessToken이 만료되면 새로 발급해줄 refreshToken 생성하기
-  public String generateRefreshToken(String subject, Date expiration, String base64EncodedSecretKey) {
-    Key key = getKeyFromBase64EncodedKey(base64EncodedSecretKey);
-
+  public String generateRefreshToken(String subject, Date expiration) {
     return Jwts.builder()
         .setSubject(subject)
         .setIssuedAt(Calendar.getInstance().getTime())
         .setExpiration(expiration)
-        .signWith(key)
+        .signWith(secretKey)
         .compact();
   }
 
-  // JWT의 서명에 사용할 Secret Key를 생성하기
-  private Key getKeyFromBase64EncodedKey(String base64EncodedSecretKey) {
-    byte[] keyBytes = Decoders.BASE64.decode(base64EncodedSecretKey);  // Base64 형식으로 인코딩된 Secret Key를 디코딩한 후, byte array를 반환
-    Key key = Keys.hmacShaKeyFor(keyBytes);    // key byte array를 기반으로 적절한 HMAC 알고리즘을 적용한 Key 객체를 생성
 
-    return key;
+  public boolean validateToken(String token) {
+    try {
+      // 서명 검증
+      verifySignature(token);
+
+      // JWT 파싱을 통해 클레임을 추출하려고 시도
+      Jwts.parserBuilder()
+          .build()
+          .parseClaimsJws(token);
+      return true;
+    } catch (ExpiredJwtException e) {
+      log.error("JWT token is expired: {}", e.getMessage());
+    } catch (UnsupportedJwtException e) {
+      log.error("JWT token is unsupported: {}", e.getMessage());
+    } catch (MalformedJwtException e) {
+      log.error("JWT token is malformed: {}", e.getMessage());
+    } catch (IllegalArgumentException e) {
+      log.error("JWT token is invalid: {}", e.getMessage());
+    } catch (SignatureException e) {
+      log.error("JWT token signature is invalid: {}", e.getMessage());
+    }
+    return false;
   }
 
-  // 토큰 검증하기
-  public void verifySignature(String jws, String base64EncodedSecretKey) {
-    Key key = getKeyFromBase64EncodedKey(base64EncodedSecretKey);
-
+  // 토큰 서명 검증하기
+  public void verifySignature(String jws) {
     Jwts.parserBuilder()
-        .setSigningKey(key) // 서명에 사용된 Secret Key를 설정
+        .setSigningKey(secretKey) // 서명에 사용된 Secret Key를 설정
         .build()
         .parseClaimsJws(jws); // JWT를 파싱해서 Claims를 얻음
+  }
+
+  // 토큰 추출하기
+  public String resolveToken(HttpServletRequest request) {
+    String bearerToken = request.getHeader("Authorization");
+    if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+      return bearerToken.substring(7); // "Bearer " 이후의 토큰 값
+    }
+    return null;
+  }
+
+  // 클레임을 추출하는 메서드
+  public Claims getClaimsFromToken(String token) {
+    return Jwts.parserBuilder()
+        .setSigningKey(secretKey)
+        .build()
+        .parseClaimsJws(token)
+        .getBody();
+  }
+
+  // 사용자 이름 추출하기
+  public String getUsernameFromToken(String token) {
+    Claims claims = getClaimsFromToken(token);
+    return claims.getSubject(); // subject가 username
+  }
+
+  // 사용자 역할 추출하기
+  public String getUserRoleFromToken(String token) {
+    Claims claims = getClaimsFromToken(token);
+    return claims.get(AUTHORIZATION_ROLE, String.class); // "role" 클레임을 추출
   }
 
 
